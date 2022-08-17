7b3e42bb463a4dbeeb9bf3dc897226fc8b961eda
Prarup Gurung <grgprarup@gmail.com>
2022-08-18 00:28:31 +0545

    Refactored, added logo
    Signed-off-by: Prarup Gurung <grgprarup@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,128 @@ class MainActivity extends StatefulWidget {
 class _MainActivityState extends State<MainActivity> {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Medical Recorder',
       theme: ThemeData(
-        primaryColor: Colors.lightBlue,
+        primarySwatch: Colors.lightBlue,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Dashboard'),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
-        drawer: buildDrawer(context),
+        drawer: _buildDrawer(context),
       ),
     );
   }
 
-  Widget buildDrawer(BuildContext context) {
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
     return Drawer(
       child: ListView(
         children: <Widget>[
-          DrawerHeader(
-            child: Text('Medical Recorder'),
-            decoration: BoxDecoration(
-              color: Colors.lightBlue,
-            ),
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+              child: Image(
+                image: AssetImage('images/logo_black.png'),
+                fit: BoxFit.cover,
+              ),
           ),
           ListTile(
             leading: Icon(Icons.home),
@@ -53,22 +149,21 @@ class _MainActivityState extends State<MainActivity> {
               Navigator.pop(context);
             },
           ),
-          buildListTile('Settings', Icons.settings),
-          buildListTile('Add New Records', Icons.add_box_rounded),
-          buildListTile('View Records', Icons.view_list),
-          buildListTile('View Doctors', Icons.view_list),
-          buildListTile('View Nurses', Icons.view_list),
-          buildListTile('View Patients', Icons.view_list),
-          buildListTile('Settings', Icons.settings),
-          buildListTile('FAQ', Icons.question_mark_rounded),
-          buildListTile('About Us', Icons.people_alt_rounded),
-          buildListTile('Contact Us', Icons.phone_in_talk_rounded),
+          _buildListTile('Add New Records', Icons.add_box_rounded),
+          _buildListTile('View Records', Icons.view_list),
+          _buildListTile('View Doctors', Icons.view_list),
+          _buildListTile('View Nurses', Icons.view_list),
+          _buildListTile('View Patients', Icons.view_list),
+          _buildListTile('Settings', Icons.settings),
+          _buildListTile('FAQ', Icons.question_mark_rounded),
+          _buildListTile('About Us', Icons.people_alt_rounded),
+          _buildListTile('Contact Us', Icons.phone_in_talk_rounded),
         ],
       ),
     );
   }
 
-  Widget buildListTile(String title, IconData icon) {
+  Widget _buildListTile(String title, IconData icon) {
     return ListTile(
       leading: Icon(icon),
       title: Text(title),
